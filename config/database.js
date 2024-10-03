9a2ff22c89baf7d34e33600f8a73ab304758c8c6
Kritika Sharma <kritika2004sgsits@gmail.com>
2024-10-04 00:28:40 +0530

    css change in index and my_profile

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Ashu@2008',
+    password: 'root',
     database: 'Localskillz'
 });
 
