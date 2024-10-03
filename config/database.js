6cf2a64566df4bcc49a793ddbbf27cfb5bda389b
Ashusingh20 <ashusinghtamra@gmail.com>
2024-10-03 14:41:34 +0530

    added css to some files

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Mansi@567',
+    password: 'Ashu@2008',
     database: 'Localskillz'
 });
 
